a2b055031ca6a628c5564621177d923756e0d07a
Andreas Sander <andreas.sander@hotmail.de>
2019-08-12 22:14:31 +0200

    Adapt Manifest file.

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,13 +40,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Contact Andreas Sander`,
+        short_name: `Andreas Sander`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#c5e4ff`,
+        theme_color: `#c5e4ff`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/me.jpg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-catch-links`,
